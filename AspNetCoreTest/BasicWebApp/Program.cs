1ce3084696b1705d01742dfe762c76b7fb075a18
nishaprasad1 <114492585+nishaprasad1@users.noreply.github.com>
2022-11-16 12:05:47 +0530

    Add files via upload

---
+++ AspNetCoreTest/BasicWebApp/Program.cs
@@ -0,0 +1,19 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.MapGet("/Welcome", DoGreeting);
+app.Run();
+
+async Task DoGreeting(HttpResponse response, string guest = "Visitor")
+{
+    await response.WriteAsync(@$"
+        <html>
+            <head>
+                <title>BasicWebApp</title>
+            </head>
+            <body>
+                <h1>Welcome {guest}</h1>
+                <b>Current Time: </b>{DateTime.Now}
+            </body>
+        </html>
+    ");
+}
