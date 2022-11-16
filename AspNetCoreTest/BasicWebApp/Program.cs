8dcf8c04824b79f51b96cd4628c286d78f8f2e19
nishaprasad1 <114492585+nishaprasad1@users.noreply.github.com>
2022-11-16 17:37:28 +0530

    Add files via upload

--- AspNetCoreTest/BasicWebApp/Program.cs
+++ AspNetCoreTest/BasicWebApp/Program.cs
@@ -1,6 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using BasicWebApp.Middlewares;
+using BasicWebApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddSingleton<ICounter, CommonCounter>();
+builder.Services.AddSingleton<ICounter, NamedCounter>();
 var app = builder.Build();
+app.UseStaticFiles();
+app.UseMiddleware<Pausing>(1);
 app.MapGet("/Welcome", DoGreeting);
+app.MapPost("/Count", DoCounting);
 app.Run();
 
 async Task DoGreeting(HttpResponse response, string guest = "Visitor")
@@ -17,3 +25,27 @@ async Task DoGreeting(HttpResponse response, string guest = "Visitor")
         </html>
     ");
 }
+
+async Task DoCounting(HttpRequest request, HttpResponse response, ICounter counter)
+{
+    string user = request.Form["guest"];
+    if(user.Length == 0)
+    {
+        response.Redirect("/formpost.html?noname=true");
+    }
+    else
+    {
+        await response.WriteAsync(@$"
+            <html>
+                <head>
+                    <title>BasicWebApp</title>
+                </head>
+                <body>
+                    <h1>Hello {user}</h1>
+                    <b>Number of Greetings: </b>{counter.CountNext(user)}
+                </body>
+            </html>
+        ");
+    }
+}
+
